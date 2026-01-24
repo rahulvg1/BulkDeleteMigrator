5f7cdf07f9a0415bf349388da1760fdd822f36dc
rahulvg1 <rahulvg0@gmail.com>
2026-01-24 15:34:25 +0530

    Fetch Bulk deletion jobs and display them

--- BulkDeleteMigrator/MyPluginControl.cs
+++ BulkDeleteMigrator/MyPluginControl.cs
@@ -1,5 +1,7 @@
 ﻿using McTools.Xrm.Connection;
 using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Messages;
+using Microsoft.Xrm.Sdk.Metadata;
 using Microsoft.Xrm.Sdk.Query;
 using System;
 using System.Collections.Generic;
@@ -9,7 +11,10 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Documents;
 using System.Windows.Forms;
+using System.Xml;
+using System.Xml.Linq;
 using XrmToolBox.Extensibility;
 
 namespace BulkDeleteMigrator
@@ -36,6 +41,8 @@ namespace BulkDeleteMigrator
             {
                 LogInfo("Settings found and loaded");
             }
+            ExecuteMethod(LoadBulkDeletionJobs);
+
         }
 
         /// <summary>
@@ -60,7 +67,136 @@ namespace BulkDeleteMigrator
             {
                 mySettings.LastUsedOrganizationWebappUrl = detail.WebApplicationUrl;
                 LogInfo("Connection has changed to: {0}", detail.WebApplicationUrl);
+                ExecuteMethod(LoadBulkDeletionJobs);
             }
+        }
+
+        private void LoadJobsButton_Click(object sender, EventArgs e)
+        {
+            ExecuteMethod(LoadBulkDeletionJobs);
+        }
+
+        private void LoadBulkDeletionJobs()
+        {
+            WorkAsync(new WorkAsyncInfo()
+            {
+                Message = "Retrieving Bulk Deletion Jobs...",
+                Work = (worker, args) =>
+                {
+                    // Operation Type - Bulk Delete
+                    const int queryOperationType = 13;
+
+                    var query = new QueryExpression("asyncoperation");
+                    query.ColumnSet.AddColumns("name", "statuscode", "recurrencepattern", "recurrencestarttime", "data");
+
+                    query.Criteria.AddCondition("operationtype", ConditionOperator.Equal, queryOperationType);
+                    query.Criteria.AddCondition("recurrencepattern", ConditionOperator.NotNull);
+
+                    query.AddOrder("createdon", OrderType.Descending);
+
+                    args.Result = Service.RetrieveMultiple(query);
+                },
+                PostWorkCallBack = (args) =>
+                {
+                    if (args.Error != null)
+                    {
+                        MessageBox.Show(args.Error.ToString(), "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                    var result = args.Result as EntityCollection;
+                    jobsDataGridView.Rows.Clear();
+                    if (result != null)
+                    {
+                        foreach (var record in result.Entities)
+                        {
+                            string name = record.GetAttributeValue<string>("name") ?? "";
+                            string status = record.FormattedValues["statuscode"];
+
+                            var recurrence = (string)record["recurrencepattern"];
+                            var recurrenceDetails = ExtractRecurrenceDetails(recurrence);
+
+                            String recurrenceStart = record.GetAttributeValue<DateTime?>("recurrencestarttime").Value.ToLocalTime().ToString("g") ?? "";
+
+                            var bulkDeleteData = (string)record["data"];
+                            var fetchXml = ExtractFetchXml(bulkDeleteData);
+
+                            var tableLogicalName = ExtractTableName(fetchXml);
+                            var tableDisplayName = GetTableDisplayName(tableLogicalName, Service);
+                            var tableNameGrid = !String.IsNullOrWhiteSpace(tableDisplayName) ?
+                            $"{tableDisplayName} ({tableLogicalName})" : tableLogicalName;
+
+                            jobsDataGridView.Rows.Add(false, name, tableNameGrid, recurrenceDetails.frequency, 
+                                recurrenceDetails.interval, recurrenceStart, status);
+
+                        }
+
+                    }
+                }
+
+            });
+        }
+
+        private string ExtractFetchXml(string bulkDeleteData)
+        {
+            XDocument doc = XDocument.Parse(bulkDeleteData);
+
+            // Find the node with the name string
+            var fetchXmlNode = doc.Descendants("string").FirstOrDefault();
+            if (fetchXmlNode != null && fetchXmlNode.Value != null)
+            {
+                return fetchXmlNode.Value;
+            }
+            return null;
+
+        }
+
+        private string ExtractTableName(string fetchXML)
+        {
+            var xmlDoc = new XmlDocument();
+            xmlDoc.LoadXml(fetchXML);
+            var entityNode = xmlDoc.SelectSingleNode("fetch/entity");
+            if (entityNode != null && entityNode.Attributes != null && entityNode.Attributes["name"] != null)
+            {
+                return entityNode.Attributes["name"].Value;
+            }
+            return null;
+
+        }
+
+        public string GetTableDisplayName(string tableLogicalName, IOrganizationService service)
+        {
+            var request = new RetrieveEntityRequest
+            {
+                EntityFilters = EntityFilters.Entity,
+                LogicalName = tableLogicalName
+            };
+
+            var response = (RetrieveEntityResponse)service.Execute(request);
+            var tableDisplayName = response.EntityMetadata.DisplayName.UserLocalizedLabel?.Label;
+            return tableDisplayName;
+        }
+
+        public (string frequency, string interval) ExtractRecurrenceDetails(string recurrencePattern)
+        {
+            string frequency = "";
+            string interval = "";
+
+            if (!String.IsNullOrWhiteSpace(recurrencePattern))
+            {
+                string[] recurrenceParts = recurrencePattern.Split(';');
+                foreach (var part in recurrenceParts)
+                {
+                    var prop = part.Split('=');
+                    if (prop[0] == "FREQ")
+                    {
+                        frequency = prop[1];
+                    }
+                    else if (prop[0] == "INTERVAL")
+                    {
+                        interval = prop[1];
+                    }
+                }
+            }
+            return (frequency, interval);
         }
     }
 }
--- BulkDeleteMigrator/MyPluginControl.designer.cs
+++ BulkDeleteMigrator/MyPluginControl.designer.cs
@@ -30,22 +30,29 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MyPluginControl));
             this.toolStripMenu = new System.Windows.Forms.ToolStrip();
-            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
             this.loadJobsButton = new System.Windows.Forms.ToolStripButton();
             this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
             this.migrateJobsButton = new System.Windows.Forms.ToolStripButton();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.currentEnvLabel = new System.Windows.Forms.Label();
-            this.targetEnvButton = new System.Windows.Forms.Button();
             this.targetEnvLabel = new System.Windows.Forms.Label();
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.targetEnvButton = new System.Windows.Forms.Button();
+            this.currentEnvLabel = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.jobsDataGridView = new System.Windows.Forms.DataGridView();
+            this.JobSelection = new System.Windows.Forms.DataGridViewCheckBoxColumn();
+            this.JobName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Table = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Frequency = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Interval = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.RecurrenceDate = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Status = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.groupBox2 = new System.Windows.Forms.GroupBox();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.toolStripMenu.SuspendLayout();
             this.tableLayoutPanel1.SuspendLayout();
             this.groupBox1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.jobsDataGridView)).BeginInit();
             this.groupBox2.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -62,30 +69,14 @@
             this.toolStripMenu.TabIndex = 4;
             this.toolStripMenu.Text = "toolStrip1";
             // 
-            // tableLayoutPanel1
-            // 
-            this.tableLayoutPanel1.ColumnCount = 1;
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
-            this.tableLayoutPanel1.Controls.Add(this.groupBox1, 0, 0);
-            this.tableLayoutPanel1.Controls.Add(this.dataGridView1, 0, 1);
-            this.tableLayoutPanel1.Controls.Add(this.groupBox2, 0, 2);
-            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 25);
-            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
-            this.tableLayoutPanel1.RowCount = 3;
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 80F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 150F));
-            this.tableLayoutPanel1.Size = new System.Drawing.Size(1215, 537);
-            this.tableLayoutPanel1.TabIndex = 5;
-            // 
             // loadJobsButton
             // 
             this.loadJobsButton.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
             this.loadJobsButton.ImageTransparentColor = System.Drawing.Color.Magenta;
             this.loadJobsButton.Name = "loadJobsButton";
-            this.loadJobsButton.Size = new System.Drawing.Size(125, 22);
-            this.loadJobsButton.Text = "Load Bulk Delete Jobs";
+            this.loadJobsButton.Size = new System.Drawing.Size(135, 22);
+            this.loadJobsButton.Text = "Reload Bulk Delete Jobs";
+            this.loadJobsButton.Click += new System.EventHandler(this.LoadJobsButton_Click);
             // 
             // toolStripSeparator1
             // 
@@ -102,6 +93,23 @@
             this.migrateJobsButton.Text = "Migrate Selected Jobs";
             this.migrateJobsButton.ToolTipText = "Migrate selected jobs to Target environment";
             // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Controls.Add(this.groupBox1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.jobsDataGridView, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.groupBox2, 0, 2);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 25);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 3;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 80F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 150F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(1215, 537);
+            this.tableLayoutPanel1.TabIndex = 5;
+            // 
             // groupBox1
             // 
             this.groupBox1.Controls.Add(this.targetEnvLabel);
@@ -116,23 +124,14 @@
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Environments";
             // 
-            // label1
+            // targetEnvLabel
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(6, 20);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(41, 13);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Source";
-            // 
-            // currentEnvLabel
-            // 
-            this.currentEnvLabel.AutoSize = true;
-            this.currentEnvLabel.Location = new System.Drawing.Point(107, 20);
-            this.currentEnvLabel.Name = "currentEnvLabel";
-            this.currentEnvLabel.Size = new System.Drawing.Size(140, 13);
-            this.currentEnvLabel.TabIndex = 1;
-            this.currentEnvLabel.Text = "Environment Name (Current)";
+            this.targetEnvLabel.AutoSize = true;
+            this.targetEnvLabel.Location = new System.Drawing.Point(107, 45);
+            this.targetEnvLabel.Name = "targetEnvLabel";
+            this.targetEnvLabel.Size = new System.Drawing.Size(137, 13);
+            this.targetEnvLabel.TabIndex = 3;
+            this.targetEnvLabel.Text = "Environment Name (Target)";
             // 
             // targetEnvButton
             // 
@@ -143,23 +142,94 @@
             this.targetEnvButton.Text = "Select Target";
             this.targetEnvButton.UseVisualStyleBackColor = true;
             // 
-            // targetEnvLabel
+            // currentEnvLabel
             // 
-            this.targetEnvLabel.AutoSize = true;
-            this.targetEnvLabel.Location = new System.Drawing.Point(107, 45);
-            this.targetEnvLabel.Name = "targetEnvLabel";
-            this.targetEnvLabel.Size = new System.Drawing.Size(137, 13);
-            this.targetEnvLabel.TabIndex = 3;
-            this.targetEnvLabel.Text = "Environment Name (Target)";
+            this.currentEnvLabel.AutoSize = true;
+            this.currentEnvLabel.Location = new System.Drawing.Point(107, 20);
+            this.currentEnvLabel.Name = "currentEnvLabel";
+            this.currentEnvLabel.Size = new System.Drawing.Size(140, 13);
+            this.currentEnvLabel.TabIndex = 1;
+            this.currentEnvLabel.Text = "Environment Name (Current)";
             // 
-            // dataGridView1
+            // label1
             // 
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.dataGridView1.Location = new System.Drawing.Point(3, 83);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(1209, 301);
-            this.dataGridView1.TabIndex = 1;
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Source";
+            // 
+            // jobsDataGridView
+            // 
+            this.jobsDataGridView.AllowUserToAddRows = false;
+            this.jobsDataGridView.AllowUserToDeleteRows = false;
+            this.jobsDataGridView.AllowUserToResizeRows = false;
+            this.jobsDataGridView.BackgroundColor = System.Drawing.SystemColors.ControlLightLight;
+            this.jobsDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.jobsDataGridView.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.JobSelection,
+            this.JobName,
+            this.Table,
+            this.Frequency,
+            this.Interval,
+            this.RecurrenceDate,
+            this.Status});
+            this.jobsDataGridView.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.jobsDataGridView.Location = new System.Drawing.Point(3, 83);
+            this.jobsDataGridView.Name = "jobsDataGridView";
+            this.jobsDataGridView.ReadOnly = true;
+            this.jobsDataGridView.RowHeadersVisible = false;
+            this.jobsDataGridView.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.jobsDataGridView.Size = new System.Drawing.Size(1209, 301);
+            this.jobsDataGridView.TabIndex = 1;
+            // 
+            // JobSelection
+            // 
+            this.JobSelection.HeaderText = " ";
+            this.JobSelection.Name = "JobSelection";
+            this.JobSelection.ReadOnly = true;
+            this.JobSelection.Width = 50;
+            // 
+            // JobName
+            // 
+            this.JobName.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.JobName.HeaderText = "Name";
+            this.JobName.Name = "JobName";
+            this.JobName.ReadOnly = true;
+            // 
+            // Table
+            // 
+            this.Table.HeaderText = "Table";
+            this.Table.Name = "Table";
+            this.Table.ReadOnly = true;
+            this.Table.Width = 300;
+            // 
+            // Frequency
+            // 
+            this.Frequency.HeaderText = "Frequency";
+            this.Frequency.Name = "Frequency";
+            this.Frequency.ReadOnly = true;
+            // 
+            // Interval
+            // 
+            this.Interval.HeaderText = "Interval";
+            this.Interval.Name = "Interval";
+            this.Interval.ReadOnly = true;
+            // 
+            // RecurrenceDate
+            // 
+            this.RecurrenceDate.HeaderText = "Start Time";
+            this.RecurrenceDate.Name = "RecurrenceDate";
+            this.RecurrenceDate.ReadOnly = true;
+            this.RecurrenceDate.Width = 150;
+            // 
+            // Status
+            // 
+            this.Status.HeaderText = "Status";
+            this.Status.Name = "Status";
+            this.Status.ReadOnly = true;
+            this.Status.Width = 150;
             // 
             // groupBox2
             // 
@@ -196,7 +266,7 @@
             this.tableLayoutPanel1.ResumeLayout(false);
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.jobsDataGridView)).EndInit();
             this.groupBox2.ResumeLayout(false);
             this.groupBox2.PerformLayout();
             this.ResumeLayout(false);
@@ -215,8 +285,15 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label targetEnvLabel;
         private System.Windows.Forms.Button targetEnvButton;
-        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView jobsDataGridView;
         private System.Windows.Forms.GroupBox groupBox2;
         private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.DataGridViewCheckBoxColumn JobSelection;
+        private System.Windows.Forms.DataGridViewTextBoxColumn JobName;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Table;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Frequency;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Interval;
+        private System.Windows.Forms.DataGridViewTextBoxColumn RecurrenceDate;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Status;
     }
 }
